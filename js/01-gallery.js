793c5cf767538e88cc14057779de62ad569e2280
JonaszChudobski <jonasz.chudobski@wp.pl>
2022-12-20 16:31:11 +0100

    just started

---
+++ js/01-gallery.js
@@ -0,0 +1,32 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryItemsLength = galleryItems.length;
+
+for (let i = 0; i < galleryItemsLength; i++) {
+  const preview = galleryItems[i]["preview"];
+  const original = galleryItems[i]["original"];
+  const description = galleryItems[i]["description"];
+  gallery.insertAdjacentHTML(
+    "afterbegin",
+    '<div class="gallery__item"><a class="gallery__link" href="large-image.jpg"><img class="gallery__image" src="small-image.jpg" data-source="large-image.jpg" alt="Image description" /></a></div>'
+  );
+  const galleryLink = document.querySelector(".gallery__link");
+  const galleryImg = document.querySelector(".gallery__image");
+  galleryLink.setAttribute("onclick", "event.preventDefault()");
+  galleryLink.setAttribute("href", original);
+  galleryImg.setAttribute("src", preview);
+  galleryImg.setAttribute("data-source", original);
+  galleryImg.setAttribute("alt", description);
+}
+
+gallery.addEventListener("click", selectImg);
+
+function selectImg(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImg = event.target.dataset.source;
+}
+
+console.log(galleryItems);
